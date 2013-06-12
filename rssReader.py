1c06cb0c4516af6a44c470b7922931b101a12de9
John Harrison <estus@Johns-MacBook-Air.local>
2013-06-12 13:31:58 -0700

    Kill the camels

--- rssReader.py
+++ rssReader.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
 import feedparser, sys, time, calendar
 
-def parseRSS(rssURL, lastRun):
+def parse_rss(rss_url, last_run):
 	# create a new parser from RSS feed
 	fp = feedparser.parse(rssURL)
 	# create list of cases needing update 
 	updates = []
 	for entry in fp.entries:
-		timeStamp = unixTime(entry.published)
+		timestamp = unix_time(entry.published)
 
 		# if more recent than last run, this entry is an update
-		if timeStamp > lastRun:
+		if timestamp > last_run:
 			case = entry.title.split(':')[0].lstrip('Case ')
 			updates.append(case)
 	return updates
 
 
 # convert fogbugz rss pubdate (RFC 822) into Unix time
-def unixTime(timestamp):
+def unix_time(timestamp):
 	return calendar.timegm(time.strptime(timestamp, "%a, %d %b %Y %H:%M:%S %Z"))
 
 
 if __name__=='__main__':
   
-  rssURL = sys.argv[1]
-  lastRun = int(sys.argv[2])
+  rss_url = sys.argv[1]
+  last_run = int(sys.argv[2])
 
-  updates = parseRSS(rssURL, lastRun)
+  updates = parse_rss(rss_url, last_run)
   for case in updates:
   	# create the json
   	# upload the doc to Cloudant
+  # update/create .ini file to reflect new lastRun state
