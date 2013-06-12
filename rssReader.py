aa7f8aca0985e086af806338a2ed67bc8ee9128a
John Harrison <estus@Johns-MacBook-Air.local>
2013-06-12 11:16:42 -0700

    Add rssReader, main script

---
+++ rssReader.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import feedparser, sys, time, calendar
+
+def parseRSS(rssURL, lastRun):
+	# create a new parser from RSS feed
+	fp = feedparser.parse(rssURL)
+	# create list of cases needing update 
+	updates = []
+	for entry in fp.entries:
+		timeStamp = unixTime(entry.published)
+
+		# if more recent than last run, this entry is an update
+		if timeStamp > lastRun:
+			case = entry.title.split(':')[0].lstrip('Case ')
+			updates.append(case)
+	return updates
+
+
+# convert fogbugz rss pubdate (RFC 822) into Unix time
+def unixTime(timestamp):
+	return calendar.timegm(time.strptime(timestamp, "%a, %d %b %Y %H:%M:%S %Z"))
+
+
+if __name__=='__main__':
+  
+  rssURL = sys.argv[1]
+  lastRun = int(sys.argv[2])
+
+  updates = parseRSS(rssURL, lastRun)
+  for case in updates:
+  	# create the json
+  	# upload the doc to Cloudant
