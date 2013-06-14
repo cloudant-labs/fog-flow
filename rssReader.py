07a87d4aa10a9c02b4a56a2639805760a1ed9012
John Harrison <estus@Johns-MacBook-Air.local>
2013-06-14 10:21:50 -0700

    Clean up, waiting on docBuilder.py

--- rssReader.py
+++ rssReader.py
@@ -1,42 +1,39 @@
 #!/usr/bin/env python
 import feedparser, sys, time, calendar, uploader
 
-def parseRSS(rssURL, lastRun):
+# parse the global rss feed for the filter, creating a list of docs to create/update
+def parseRSS(rss_URL, last_run):
 	# create a new parser from RSS feed
 	fp = feedparser.parse(rssURL)
 	# create list of cases needing update 
 	updates = []
 	for entry in fp.entries:
-		timeStamp = unixTime(entry.published)
+		timestamp = unixTime(entry.published)
 
 		# if more recent than last run, this entry has an update
-		if timeStamp > lastRun:
+		if timeStamp > last_run:
 			case = entry.title.split(':')[0].lstrip('Case ')
 			updates.append(case)
 	return updates
 
 
-# convert fogbugz rss pubdate into Unix time
+# convert fogbugz rss date into Unix timestamp
 def unixTime(timestamp):
 	return calendar.timegm(time.strptime(timestamp, "%a, %d %b %Y %H:%M:%S %Z"))
 
 
 if __name__=='__main__':
-	rssURL = sys.argv[1]
-	lastRun = int(sys.argv[2])
+	rss_URL = sys.argv[1]
+	last_run = int(sys.argv[2])
 	updates = ["1"]
 	#  updates = parseRSS(rssURL, lastRun)
 	success = []
 	for case in updates:
 		# create the json
-  		json_doc = {"_id": "2062332", "foo": "bar"}
+		# call to docBuilder function here
   		u = uploader.upload(json_doc)
   		if u:
-	  		""" should let us get a list of which succeeded 
-	  		may be more helpful to have list of failures; 
-	  		picking out values that dont appear in both lists
-	  		that we can report 
-	  		"""
   			success.append(u)
-  	# success if lengths match
+  	# success if lengths match (as many successes as docs)
   	print len(success) == len(updates)
+  	### update last_run (or I suppose Cron handles this) ###
--- uploader.py
+++ uploader.py
@@ -4,6 +4,5 @@ import json, requests, fbSettings
 def upload(json_doc):
 	headers = {"content-type": "application/json"}
 	resp = requests.post(fbSettings.DB_URL, auth=(fbSettings.DB_USER, fbSettings.DB_PASS), data=json.dumps(json_doc), headers=headers)
-	# print resp.prettify() # debugging
 	# will return true if this upload was successful
 	return resp.status_code < 300
