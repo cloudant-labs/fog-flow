c0b2b5cad1dc4484cd622e67d8e90a664744018d
John Harrison <estus@Johns-MacBook-Air.local>
2013-06-17 16:45:43 -0700

    incorporate temp file for most recent execution, updating state

--- rssReader.py
+++ rssReader.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-import feedparser, sys, time, calendar, uploader, docBuilder, fbSettings
+#!/usr/bin/python
+import feedparser, sys, time, calendar, uploader, docBuilder, fbSettings, json
 
 # parse the global rss feed for the filter, creating a list of docs to create/update
 def parse_rss(rss_url, last_run):
@@ -17,14 +17,41 @@ def parse_rss(rss_url, last_run):
 	return updates
 
 
+# reads a Unix timestamp from a temp file representing the last time
+# at which this script was run, then updates
+# if no such file exists, returns Unix epoch as an integer (0)
+def get_last_run():
+	try:
+		# check for the file
+		with open('/tmp/fog-flow-state.json'):
+			json_data = open('/tmp/fog-flow-state.json', 'r')
+			data = json.load(json_data)
+			# obtain current state
+			last_run = data['last_run']
+			# update state to reflect new execution
+			json_data.close()
+			return int(last_run)
+	except IOError:
+		return 0
+
+
+# updates the timestamp by overwriting/creating the temp state file
+def update_last_run():
+	f = open('/tmp/fog-flow-state.json', 'w')
+	data = {}
+	data['last_run'] = calendar.timegm(time.gmtime())
+	f.write(json.dumps(data))
+
+
 # convert fogbugz rss date into Unix timestamp
 def unixTime(timestamp, format):
 	return calendar.timegm(time.strptime(timestamp, format))
 
 
 if __name__=='__main__':
-	rss_url = sys.argv[1]
-	last_run = int(sys.argv[2])
+	rss_url = fbSettings.RSS_URL
+	last_run = get_last_run()
+	update_last_run()
 	updates = parse_rss(rss_url, last_run)
 	success = []
 	for case_id in updates:
@@ -38,4 +65,3 @@ if __name__=='__main__':
   			success.append(u)
   	# success if lengths match (as many successes as docs)
   	print len(success) == len(updates)
-  	### update last_run (or I suppose Cron handles this) ###
