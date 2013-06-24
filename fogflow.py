3c9035a0d5cf2425afe22e07a18fac9d1f096ec5
John Harrison <john@cloudant.com>
2013-06-24 15:47:01 -0700

    Consolidate rssreader and docbuilder and rename to fogflow.py

--- fogflow.py
+++ fogflow.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+
 import calendar
 import ConfigParser
 import json
-import re
+import sys
 import time
 
+import feedparser
 from fogbugz import FogBugz
 import requests
 import xmltodict
 
-TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+FB_TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 def unix_time(timestamp, format):
     if timestamp:
@@ -41,10 +44,10 @@ def prune_doc(doc):
     if d['tags']:
         d['tags'] = d['tags']['tag']
     d['events'] = [prune_event(e) for e in d['events']['event']]
-    d['last_updated'] = unix_time(d['last_updated'], TIME_FORMAT)
-    d['date_opened'] = unix_time(d['date_opened'], TIME_FORMAT)
-    d['date_closed'] = unix_time(d['date_closed'], TIME_FORMAT)
-    d['date_resolved'] = unix_time(d['date_resolved'], TIME_FORMAT)
+    d['last_updated'] = unix_time(d['last_updated'], FB_TIME_FORMAT)
+    d['date_opened'] = unix_time(d['date_opened'], FB_TIME_FORMAT)
+    d['date_closed'] = unix_time(d['date_closed'], FB_TIME_FORMAT)
+    d['date_resolved'] = unix_time(d['date_resolved'], FB_TIME_FORMAT)
     return d
 
 
@@ -58,7 +61,7 @@ def prune_event(event):
         for key, value in event.items()
         if key not in excludekeys
     ])
-    e['timestamp'] = unix_time(e['timestamp'], TIME_FORMAT)
+    e['timestamp'] = unix_time(e['timestamp'], FB_TIME_FORMAT)
     return e
 
 
@@ -110,7 +113,7 @@ def transform(key):
         return key
 
 
-def build(api_url, case_id, api_user, api_pass, db_url, db_user, db_pass):
+def build_doc(api_url, case_id, api_user, api_pass, db_url, db_user, db_pass):
     fb = FogBugz(api_url)
     fb.logon(api_user, api_pass)
     case = fb.search(
@@ -128,4 +131,79 @@ def build(api_url, case_id, api_user, api_pass, db_url, db_user, db_pass):
     fb.logoff()
     print json.dumps(doc, indent=4, separators=(",", "; "))
     return doc
+
+
+def parse_rss(rss_url, last_run):
+    fp = feedparser.parse(rss_url)
+    updates = []
+    for entry in fp.entries:
+        timestamp = unix_time(
+            entry.published,
+            "%a, %d %b %Y %H:%M:%S %Z"
+        )
+        if timestamp > last_run:
+            case = entry.title.split(':')[0].lstrip('Case ')
+            updates.append(case)
+    return updates
+
+
+def upload_doc(json_doc, db_url, db_user, db_pass):
+    headers = {"content-type": "application/json"}
+    resp = requests.post(
+        db_url,
+        auth=(db_user, db_pass),
+        data=json.dumps(json_doc),
+        headers=headers
+    )
+    return resp.status_code in [201, 202]
+
+
+def get_last_run(tempfile):
+    try:
+        with open(tempfile, 'r') as json_data:
+            data = json.load(json_data)
+            return int(data['last_run'])
+    except IOError:
+        return 0
+
+
+def update_last_run(current_run, tempfile):
+    with open(tempfile, 'w') as f:
+        data = {}
+        data['last_run'] = current_run
+        json.dump(data, f)
+
+
+def main(configfile):
+    config = ConfigParser.RawConfigParser()
+    config.read(configfile)
+    current_run = calendar.timegm(time.gmtime())
+    rss_url = config.get('FogBugz', 'rss_url')
+    api_url = config.get('FogBugz', 'api_url')
+    api_user = config.get("FogBugz", 'api_user')
+    api_pass = config.get("FogBugz", 'api_pass')
+    db_url = config.get("Cloudant", 'db_url')
+    db_user = config.get("Cloudant", 'db_user')
+    db_pass = config.get("Cloudant", 'db_pass')
+    tempfile = config.get("LastRun", 'tempfile')
+    last_run = get_last_run(tempfile)
+    for case_id in parse_rss(rss_url, last_run):
+        json_doc = build_doc(
+            api_url,
+            case_id,
+            api_user,
+            api_pass,
+            db_url,
+            db_user,
+            db_pass
+        )
+        if not upload_doc(json_doc, db_url, db_user, db_pass):
+            sys.exit(1)
+    print "successful"
+    update_last_run(current_run, tempfile)
+
+
+if __name__=='__main__':
+    configfile = sys.argv[1]
+    main(configfile)
 
