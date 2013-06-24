ae3790996dcf647bf48569b7bd7065062a942866
John Harrison <john@cloudant.com>
2013-06-24 15:04:09 -0700

    Break up doc-pruning, add command line arg for ini file

--- docbuilder.py
+++ docbuilder.py
@@ -10,7 +10,6 @@ import xmltodict
 
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
-# convert fogbugz datetime into Unix timestamp
 def unix_time(timestamp, format):
     if timestamp:
         return calendar.timegm(time.strptime(timestamp, format))
@@ -19,71 +18,92 @@ def unix_time(timestamp, format):
 
 
 def get_rev(case_id, db_url, db_user, db_pass):
-    r = requests.head(db_url + case_id,
-                    auth=(db_user, db_pass))
-    if r.status_code == 200 or r.status_code == 202:
+    r = requests.head(
+        db_url + case_id,
+        auth=(db_user, db_pass)
+    )
+    if r.status_code == 200:
         return r.headers['etag'].strip('"')
     else:
         return None
 
 
-# collapse unwanted xml fields, revise names, reformat timestamps
 def prune_doc(doc):
-    doc = doc['response']['cases']['case']
-    doc['events'] = doc['events']['event']
-    del doc['@operations'], doc['ixpersonopenedby']
-    del doc['ixpersonclosedby'], doc['ixpersonresolvedby']
-    del doc['ixpersonlasteditedby'], doc['plugin_customfields']
-    if doc['tags']:
-        doc['tags'] = doc['tags']['tag']
-    events = []
-    for event in reversed(doc['events']):
-        events.append(dict([(transform(key), value)
-                     for key, value in event.items()]))
-    for event in events:
-        event['timestamp'] = unix_time(event['timestamp'], TIME_FORMAT)
-        del event['fhtml'], event['_id'], event['bemail']
-        del event['ixbugevent'], event['sformat'], event['ixperson']
-        del event['ixpersonassignedto'], event['bexternal']
-        if 'shtml' in event: del event['shtml']
-    doc['events'] = events
-    doc = dict([(transform(key), value) for key, value in doc.items()])
-    doc['last_updated'] = unix_time(doc['last_updated'], TIME_FORMAT)
-    doc['date_opened'] = unix_time(doc['date_opened'], TIME_FORMAT)
-    doc['date_closed'] = unix_time(doc['date_closed'], TIME_FORMAT)
-    doc['date_resolved'] = unix_time(doc['date_resolved'], TIME_FORMAT)
-    print json.dumps(doc, indent=4, separators=(',', ': '))
-    return doc
+    excludekeys = [
+        '@operations', 'ixpersonopenedby', 'ixpersonclosedby',
+        'ixpersonresolvedby', 'ixpersonlasteditedby', 'plugin_customfields'
+    ]
+    d = dict([(
+        transform(key), value)
+        for key, value in doc['response']['cases']['case'].items()
+        if key not in excludekeys
+    ])
+    if d['tags']:
+        d['tags'] = d['tags']['tag']
+    d['events'] = [prune_event(e) for e in d['events']['event']]
+    d['last_updated'] = unix_time(d['last_updated'], TIME_FORMAT)
+    d['date_opened'] = unix_time(d['date_opened'], TIME_FORMAT)
+    d['date_closed'] = unix_time(d['date_closed'], TIME_FORMAT)
+    d['date_resolved'] = unix_time(d['date_resolved'], TIME_FORMAT)
+    return d
 
 
-# substitute ambiguous key names from fb for preferable names for clarity
+def prune_event(event):
+    excludekeys = [
+        'ixbugevent', 'ixpersonassignedto', 'shtml', 'fhtml', 'smessageid',
+        '@ixbug', 'bemail', 'sformat', 'ixperson', 'bexternal'
+    ]
+    e = dict([
+        (transform(key), value)
+        for key, value in event.items()
+        if key not in excludekeys
+    ])
+    e['timestamp'] = unix_time(e['timestamp'], TIME_FORMAT)
+    return e
+
+
 def transform(key):
-    renamings = dict([('@ixbug', '_id'), ('ixpriority', 'priority'), 
-                    ('ixrelatedbugs', 'related_cases'), ('stitle', 'title'),
-                    ('dtopened', 'date_opened'), ('dtclosed', 'date_closed'),
-                    ('spersonassignedto', 'assigned_to'), ('scc', 'cc'),
-                    ('sstatus', 'status'), ('ixbugparent', 'parent_case'),
-                    ('dtresolved', 'date_resolved'), ('sproject', 'project'),
-                    ('sarea', 'area'), ('ixbugchildren', 'sub_cases'),
-                    ('dtlastupdated', 'last_updated'), ('ssubject', 'subject'),
-                    ('scategory', 'category'), ('events', 'events'),
-                    ('@ixbugevent', 'event_id'), ('evt', 'event_code'),
-                    ('sverb', 'event_type'),('dt', 'timestamp'), ('sto', 'to'),
-                    ('s', 'event_text'), ('femail', 'is_email'),
-                    ('fexternal', 'externally_triggered'), ('sfrom', 'from'),
-                    ('schanges', 'changes'), ('rgattachments', 'attachments'),
-                    ('evtdescription', 'event_description'), ('sperson', 'person'),
-                    ('sbcc', 'bcc'), ('sreplyto', 'replyto'), ('sdate', 'send_date'),
-                    ('sbodyhtml', 'body_html'), ('sbodytext', 'body_text')])
-    # hack to get around gross FB api representation of custom fields
+    renamings = {
+        '@ixbug': '_id', 'ixpriority': 'priority',
+        'ixrelatedbugs': 'related_cases', 'stitle': 'title',
+        'dtopened': 'date_opened', 'dtclosed': 'date_closed',
+        'spersonassignedto': 'assigned_to', 'scc': 'cc',
+        'sstatus': 'status', 'ixbugparent': 'parent_case',
+        'dtresolved': 'date_resolved', 'sproject': 'project',
+        'sarea': 'area', 'ixbugchildren': 'sub_cases',
+        'dtlastupdated': 'last_updated', 'ssubject': 'subject',
+        'scategory': 'category', 'events': 'events',
+        '@ixbugevent': 'event_id', 'evt': 'event_code',
+        'sverb': 'event_type', 'dt': 'timestamp',
+        'sto': 'to', 's': 'event_text', 'femail': 'is_email',
+        'fexternal': 'externally_triggered', 'sfrom': 'from',
+        'schanges': 'changes', 'rgattachments': 'attachments',
+        'evtdescription': 'event_description', 'sperson': 'person',
+        'sbcc': 'bcc', 'sreplyto': 'replyto', 'sdate': 'send_date',
+        'sbodyhtml': 'body_html', 'sbodytext': 'body_text'
+    }
+    """
+    The FB api does not yet have a method of returning the
+    literal values of user defined keys, and returns user-defined custom fields
+    in a poor format in which whitespace is represented as 'x', however any
+    'x' characters in the key itself are not escaped, leading to some ambiguity
+    This is an attempt to clean up some of these fields, but is likely to fail
+    in several cases involving multiple 'x' chars in a key
+
+    FB 9.0 promises a list_custom_fields function in its XML API, but have
+    to wait until then...
+    """ 
+    # remove leading fogcreek labels
     if key.startswith('plugin_customfields_at_fogcreek_com_'):
-        result = key.replace('plugin_customfields_at_fogcreek_com_', '')[:-2]
-        ending = result[-1:]
-        if re.match('[0-9]', ending):
+        result = key.replace(
+            'plugin_customfields_at_fogcreek_com_', '')[:-2].replace('x', '_')
+        if "__" in result:
+            result = result.replace('__', '_x')
+        # remove odd character sequence at end of key
+        if result[-1:].isdigit():
             return result[:-2]
         else:
             return result[:-1]
-    # end grossness
     if key in renamings.keys():
         return renamings[key]
     else:
@@ -93,11 +113,19 @@ def transform(key):
 def build(api_url, case_id, api_user, api_pass, db_url, db_user, db_pass):
     fb = FogBugz(api_url)
     fb.logon(api_user, api_pass)
-    case = fb.search(q=str(case_id), cols='sTitle,dtOpened,dtClosed,ixPersonOpenedBy,ixPersonClosedBy,ixPersonResolvedBy,ixPersonLastEditedBy,ixRelatedBugs,sPersonAssignedTo,sStatus,ixPriority,tags,ixBugParent,ixBugChildren,dtResolved,dtClosed,dtLastUpdated,sProject,sArea,sCategory,events,plugin_customfields')
+    case = fb.search(
+        q=str(case_id),
+        cols='sTitle,dtOpened,dtClosed,ixPersonOpenedBy,ixPersonClosedBy,'
+        'ixPersonResolvedBy,ixPersonLastEditedBy,ixRelatedBugs,'
+        'sPersonAssignedTo,sStatus,ixPriority,tags,ixBugParent,'
+        'ixBugChildren,dtResolved,dtClosed,dtLastUpdated,sProject,'
+        'sArea,sCategory,events,plugin_customfields'
+    )
     doc = prune_doc(xmltodict.parse(str(case)))
-    # add rev if the document is a revision
     rev = get_rev(doc['_id'], db_url, db_user, db_pass)
     if rev:
         doc['_rev'] = rev
-    fb.logoff() # log off from fogbugz
+    fb.logoff()
+    print json.dumps(doc, indent=4, separators=(",", "; "))
     return doc
+
--- rssreader.py
+++ rssreader.py
@@ -11,49 +11,40 @@ import requests
 
 import docbuilder
 
-# to parse the global rss feed for the filter,
-# creating a list of docs to create/update
 def parse_rss(rss_url, last_run):
-    # create a new parser from RSS feed
     fp = feedparser.parse(rss_url)
-    # create list of cases needing update
     updates = []
     for entry in fp.entries:
-        timestamp = docbuilder.unix_time(entry.published,
-                                        "%a, %d %b %Y %H:%M:%S %Z")
-
-        # if more recent than last run, this entry has an update
+        timestamp = docbuilder.unix_time(
+            entry.published,
+            "%a, %d %b %Y %H:%M:%S %Z"
+        )
         if timestamp > last_run:
             case = entry.title.split(':')[0].lstrip('Case ')
             updates.append(case)
     return updates
 
-# upload an individual json document
+
 def upload(json_doc, db_url, db_user, db_pass):
     headers = {"content-type": "application/json"}
-    resp = requests.post(db_url, auth=(db_user, db_pass),
-                        data=json.dumps(json_doc),
-                        headers=headers)
-    # will return true if this upload was successful
+    resp = requests.post(
+        db_url,
+        auth=(db_user, db_pass),
+        data=json.dumps(json_doc),
+        headers=headers
+    )
     return resp.status_code in [201, 202]
 
 
-
-# reads a Unix timestamp from a temp file representing the last time
-# at which this script was run, then updates
-# if no such file exists, returns Unix epoch as an integer (0)
 def get_last_run(tempfile):
     try:
-        # check for the file
         with open(tempfile, 'r') as json_data:    
             data = json.load(json_data)
-            # obtain current state
             return int(data['last_run'])
     except IOError:
         return 0
 
 
-# updates the timestamp by overwriting/creating the temp state file
 def update_last_run(current_run, tempfile):
     with open(tempfile, 'w') as f:
         data = {}
@@ -61,41 +52,38 @@ def update_last_run(current_run, tempfile):
         json.dump(data, f)
 
 
-# helper function for ConfigParser
-def ConfigSectionMap(section, Config):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                DebugPrint("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
-
-
-def main():
-    Config = ConfigParser.ConfigParser()
-    Config.read("flow.ini")
+def main(configfile):
+    config = ConfigParser.RawConfigParser()
+    config.read(configfile)
     current_run = calendar.timegm(time.gmtime())
-    rss_url = ConfigSectionMap("FogBugzAPI", Config)['rss_url']
-    api_url = ConfigSectionMap("FogBugzAPI", Config)['api_url']
-    api_user = ConfigSectionMap("FogBugzAPI", Config)['api_user']
-    api_pass = ConfigSectionMap("FogBugzAPI", Config)['api_pass']
-    db_url = ConfigSectionMap("Cloudant", Config)['db_url']
-    db_user = ConfigSectionMap("Cloudant", Config)['db_user']
-    db_pass = ConfigSectionMap("Cloudant", Config)['db_pass']
-    tempfile = ConfigSectionMap("LastRun", Config)['tempfile']
+    rss_url = config.get('FogBugz', 'rss_url')
+    api_url = config.get('FogBugz', 'api_url')
+    api_user = config.get("FogBugz", 'api_user')
+    api_pass = config.get("FogBugz", 'api_pass')
+    db_url = config.get("Cloudant", 'db_url')
+    db_user = config.get("Cloudant", 'db_user')
+    db_pass = config.get("Cloudant", 'db_pass')
+    tempfile = config.get("LastRun", 'tempfile')
     last_run = get_last_run(tempfile)
     for case_id in parse_rss(rss_url, last_run):
-        json_doc = docbuilder.build(api_url, case_id, api_user, api_pass, db_url, db_user, db_pass)
+        json_doc = docbuilder.build(
+            api_url,
+            case_id,
+            api_user,
+            api_pass,
+            db_url,
+            db_user,
+            db_pass
+        )
         if not upload(json_doc, db_url, db_user, db_pass):
             sys.exit(1)
+    print "successful"
     update_last_run(current_run, tempfile)
-    print "success"
 
 
 if __name__=='__main__':
-    main()
+    configfile = sys.argv[1]
+    main(configfile)
+
+
+
